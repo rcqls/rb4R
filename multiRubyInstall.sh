cded1186094e8a89d7837216e96e7d541336691b
R cqls <rdrouilh@gmail.com>
2014-10-27 15:56:57 +0100

    see dyndoc/doc/ruby_version_and_R_versions.README

--- multiRubyInstall.sh
+++ multiRubyInstall.sh
@@ -2,10 +2,10 @@
 
 source /usr/local/opt/chruby/share/chruby/chruby.sh
 
-for RUBY in 2.0 2.1 #rbx unfortunately fails from now since no static or shared library
+for RUBY in 2.0.0-p353 2.0.0-p481 2.0.0-p576 2.1.3 #rbx unfortunately fails from now since no static or shared library
 do
 	chruby $RUBY
-	path=$(ruby -e "puts File.join(ENV[\"HOME\"],\"dyndoc\",\"R\",\"library\",RUBY_ENGINE,RUBY_VERSION)")
+	path=$(ruby -e "puts File.join(ENV[\"HOME\"],\"dyndoc\",\"R\",\"library\",RUBY_ENGINE,RbConfig::CONFIG[\"exec_prefix\"].split(File::Separator)[-1])")
 	mkdir -p $path
 	R CMD INSTALL --preclean  ~/Github/rb4R -l $path
 done
